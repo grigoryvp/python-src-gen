8dec9105c5a84ce80e0a1dd806f93003716c7e7d
Grigory Petrov <grigory.v.p@gmail.com>
2019-04-19 12:10:22 +0300

    oop

--- app.py
+++ app.py
@@ -2,6 +2,22 @@ from pathlib import Path
 import click
 
 
+class App:
+    def __init__(self, out_dir):
+        self.out_path = Path(out_dir)
+
+
+    def generate(self):
+        self._prepare_dir()
+
+
+    def _prepare_dir(self):
+        try:
+            self.out_path.mkdir()
+        except FileExistsError:
+            pass
+
+
 @click.group()
 def cli(): pass
 
@@ -11,11 +27,8 @@ def cli(): pass
     type=click.Path(resolve_path=True),
     help="Directory path where to generate output")
 def generate(out_dir):
-    root = Path(out_dir)
-    try:
-        root.mkdir()
-    except FileExistsError:
-        pass
+    app = App(out_dir=out_dir)
+    app.generate()
 
 
 cli()

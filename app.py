f6b2907085ef4e49114bc378850d4a96187d487b
Grigory Petrov <grigory.v.p@gmail.com>
2019-04-19 12:07:09 +0300

    App dir creation

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
+from pathlib import Path
 import click
 
-@click.command() # Provides '--help'
+
+@click.group()
 def cli(): pass
+
+
+@cli.command()
+@click.option('--out-dir', default='./out',
+    type=click.Path(resolve_path=True),
+    help="Directory path where to generate output")
+def generate(out_dir):
+    root = Path(out_dir)
+    try:
+        root.mkdir()
+    except FileExistsError:
+        pass
+
+
 cli()

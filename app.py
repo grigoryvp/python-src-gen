63b9580a6f6a2c30e2ffc12f41b6d7c2a3a7917a
Grigory Petrov <grigory.v.p@gmail.com>
2019-04-20 15:00:17 +0300

    Implementation

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from shutil import rmtree
+from uuid import uuid4
+import re
 import click
 
 
@@ -9,13 +12,32 @@ class App:
 
     def generate(self):
         self._prepare_dir()
+        self._generate_file('begin.py', 'end.py')
+        # Generate a number of file that import each other.
+        self._generate_file('end.py')
 
 
     def _prepare_dir(self):
         try:
+            rmtree(str(self.out_path), ignore_errors=True)
             self.out_path.mkdir()
         except FileExistsError:
             pass
+
+
+    def _generate_file(self, name, import_name: str=None):
+        path = self.out_path / name
+        path.touch()
+        text = ""
+        if import_name:
+            module_name = re.sub('\.py$', '', import_name)
+            text += f'import {module_name}'
+        text += '\n\ndef test(arg: int):'
+        if import_name:
+            text += f'\n    {module_name}.test(arg)'
+        else:
+            text += '\n    pass'
+        path.write_text(text)
 
 
 @click.group()

5e75e44bae18d1573079f721b10730aab45718a6
Grigory Petrov <grigory.v.p@gmail.com>
2019-04-21 18:18:17 +0300

    Configurable number of files, default to ~ stack trace maximum

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ import click
 
 
 class App:
-    def __init__(self, out_dir):
+    def __init__(self, out_dir, num_files):
         self.out_path = Path(out_dir)
+        self.num_files = num_files
 
 
     def generate(self):
         self._prepare_dir()
-        num_files = 2
         next_name = 'file_00000000.py'
         self._generate_file('begin.py', next_name)
-        for file_idx in range(num_files - 1):
+        for file_idx in range(self.num_files - 1):
             cur_name = next_name
             next_name = f'file_{str(file_idx + 1).zfill(8)}.py'
             self._generate_file(cur_name, next_name)
@@ -54,8 +54,10 @@ def cli(): pass
 @click.option('--out-dir', default='./out',
     type=click.Path(resolve_path=True),
     help="Directory path where to generate output")
-def generate(out_dir):
-    app = App(out_dir=out_dir)
+@click.option('--num-files', default=120,
+    help="Number of intermediate files to generate")
+def generate(out_dir, num_files):
+    app = App(out_dir=out_dir, num_files=num_files)
     app.generate()
 
 
